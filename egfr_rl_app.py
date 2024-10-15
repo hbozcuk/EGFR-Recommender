30b715d6a5441cd44e9985c985e451ab6eb4f006
Prof. Dr. Hakan Şat Bozcuk <46084724+hbozcuk@users.noreply.github.com>
2024-10-15 19:08:58 +0300

    Add files via upload

--- egfr_rl_app.py
+++ egfr_rl_app.py
@@ -150,10 +150,10 @@ def recommend_treatment(patient_features, previous_treatment_value):
     else:
         # If previous treatment = 0, favor action 1 more and penalize action 0
         if previous_treatment_value == 0:
-            q_values[0] -= 20  # Penalize action 0
-            q_values[1] += 15  # Favor action 1
-            q_values[2] += 8   # Slightly favor action 2
-            q_values[3] += 20  # Strongly favor action 3
+            q_values[0] -= 25  # Penalize action 0
+            q_values[1] += 20  # Favor action 1
+            q_values[2] += 6   # Slightly favor action 2
+            q_values[3] += 30  # Strongly favor action 3
         
         # Use Boltzmann exploration with tau=8 for action selection among all actions (0, 1, 2, 3)
         recommended_action = boltzmann_action_selection(q_values, tau=8)
